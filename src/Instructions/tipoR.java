ee2e8d928d529ce470e4d43c37a6988274c56326
15105103 <15105103@localhost>
2016-05-06 19:02:33 -0300

    Versão segura para importar

--- src/Instructions/tipoR.java
+++ src/Instructions/tipoR.java
@@ -4,7 +4,6 @@ public class tipoR extends Instruction{
     protected final String rd;
     protected final String shamt;
     protected final String funct;
-    protected final String teste;
     
     public tipoR(String newOpcode, String newRs, String newRt, String newRd, String newShamt, String newFunct){
         super(newOpcode, newRs, newRt);
